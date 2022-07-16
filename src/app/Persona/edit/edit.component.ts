80df2643f4f3bab8c0fe057cd23b221c906f5b23
duvan.botero <duvis07>
2022-07-16 16:02:08 -0500

    update

--- src/app/Persona/edit/edit.component.ts
+++ src/app/Persona/edit/edit.component.ts
@@ -25,36 +25,45 @@ export class EditComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void { 
+  ngOnInit(): void {
     this.editar();
   }
 
   editar() {
-    let id:string| any; 
-   id = localStorage.getItem('id');
-   this.service.getPersonaId(id).subscribe((data) => {
-    this.service.updatePersona(data).subscribe((data) => {
-      this.persona = data;
+    let id: string | any;
+    id = localStorage.getItem('id');
+    this.service.getPersonaId(id).subscribe((data) => {
+      this.service.updatePersona(data).subscribe((data) => {
+        this.persona = data;
+      });
     });
-   });
- } 
-
-actualizar(persona: Persona) {
-  const person:Persona = {
-    id: persona.id,
-    nombre: this.editPersona.value.nombre=== '' ? this.persona.nombre:this.editPersona.value.nombre,
-    apellido: this.editPersona.value.apellido === '' ? this.persona.apellido:this.editPersona.value.apellido,
-    edad: this.editPersona.value.edad=== '' ? this.persona.edad:this.editPersona.value.edad,
-    nacionalidad: this.editPersona.value.nacionalidad=== '' ? this.persona.nacionalidad:this.editPersona.value.nacionalidad,
   }
 
-  this.service.updatePersona(person).subscribe((data) => {
-    this.persona = data;
-    alert('Se actualizo correctamente el usuario');
-    this.router.navigate(['/listar']);
-  });
-}
- 
+  actualizar(persona: Persona) {
+    const person: Persona = {
+      id: persona.id,
+      nombre:
+        this.editPersona.value.nombre === ''
+          ? this.persona.nombre
+          : this.editPersona.value.nombre,
+      apellido:
+        this.editPersona.value.apellido === ''
+          ? this.persona.apellido
+          : this.editPersona.value.apellido,
+      edad:
+        this.editPersona.value.edad === ''
+          ? this.persona.edad
+          : this.editPersona.value.edad,
+      nacionalidad:
+        this.editPersona.value.nacionalidad === ''
+          ? this.persona.nacionalidad
+          : this.editPersona.value.nacionalidad,
+    };
 
+    this.service.updatePersona(person).subscribe((data) => {
+      this.persona = data;
+      alert('Se actualizo correctamente el usuario');
+      this.router.navigate(['/listar']);
+    });
+  }
 }
-
